e58232b1d31abb98a36a7541c69ad734cf60b258
Krzysztof Łopuch <klopuch@gmail.com>
2026-09-19 12:55:46 +0200

    chore - add rabbitmq

---
+++ src/services/HrAgencySystem.NotificationWorker/Program.cs
@@ -0,0 +1,7 @@
+using HrAgencySystem.NotificationWorker;
+
+var builder = Host.CreateApplicationBuilder(args);
+builder.Services.AddHostedService<Worker>();
+
+var host = builder.Build();
+host.Run();
